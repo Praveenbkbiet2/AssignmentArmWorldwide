579ad811fad06372b5de624bbb57c87eb9f0ac2d
Praveenbkbiet2 <praveen.bkbiet2@gmail.com>
2025-09-12 15:22:19 +0530

    Initial Commit

---
+++ AssignmentArmWorldwide/AssignmentArmWorldwideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentArmWorldwideApp.swift
+//  AssignmentArmWorldwide
+//
+//  Created by Praveen Verma on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentArmWorldwideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
